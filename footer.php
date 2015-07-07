a209f4d9113799d77792f81dc454717e0f9d90ca
andrea rota <a.rota@lse.ac.uk>
2015-07-07 16:13:33 +0100

    code formatting

--- footer.php
+++ footer.php
@@ -14,11 +14,11 @@
 			<div class="row">
 				<div class="container logos">	
 					<div class="col-xs-12 col-md-10">A collaboration between:<br>
-						<img src="
-<?php bloginfo('template_directory'); ?>/img/logo_lsecities.png" alt="LSE Cities">
+						<img src="<?php bloginfo('template_directory'); ?>/img/logo_lsecities.png" alt="LSE Cities">
 						<img src="<?php bloginfo('template_directory'); ?>/img/logo_unhabitat.png" alt="UN Habitat">
 						<img src="<?php bloginfo('template_directory'); ?>/img/logo_uclg_en.png" alt="UCLG"></div>
-					<div class="col-xs-12 col-md-2 right">Supported by: <br> <img src="<?php bloginfo('template_directory'); ?>/img/logo_macarthur.png" alt="MacArthur Foundation"></div>
+					<div class="col-xs-12 col-md-2 right">Supported by:<br>
+						<img src="<?php bloginfo('template_directory'); ?>/img/logo_macarthur.png" alt="MacArthur Foundation"></div>
 				</div>
 			</div>
 		</div>
