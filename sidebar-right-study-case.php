aa16301786b1c312aebcd9611336f148b3d6d6d3
andrea rota <a.rota@lse.ac.uk>
2015-07-07 19:19:46 +0100

    only display labels if needed; adapt label to item count

--- sidebar-right-study-case.php
+++ sidebar-right-study-case.php
@@ -3,20 +3,27 @@
 					<div class="details">
 						<h4 class="title">Case study details</h4>
 						<?php 
-						$authors = get_post_meta($post->ID, 'Author', false); 
-						$institutions = get_post_meta($post->ID, 'Institution', false); ?>
-						<h4>Author/s</h4>
+						$authors = get_post_meta($post->ID, 'Author', false);
+						$authors_label = (1 == count($authors)) ? 'Author' : 'Authors';
+
+						$institutions = get_post_meta($post->ID, 'Institution', false);
+						$institutions_label = (1 == count($institutions)) ? 'Institution' : 'Institutions'; ?>
+						<?php if(!empty($authors)) : ?>
+						<h4><?php echo $authors_label; ?></h4>
 						<ul>
 						<?php foreach($authors as $author) {
 							echo '<li>'.$author.'</li>';
 							} ?>				
 						</ul>
-						<h4>Institution</h4>
+						<?php endif; // (!empty($authors)) ?>
+						<?php if(!empty($institutions)) : ?>
+						<h4><?php echo $institutions_label; ?></h4>
 						<ul>
 						<?php foreach($institutions as $institution) {
 							echo '<li>'.$institution.'</li>';
 							} ?>
 						</ul>
+						<?php endif; // (!empty($institutionss)) ?>
 						<?php do_action('before_sidebar'); ?> 
 						<?php dynamic_sidebar('sidebar-right-study-case'); ?>
 					</div>
