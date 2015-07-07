0bbe7a45f8ad8a41f1ab96360792694cf90e4467
andrea rota <a.rota@lse.ac.uk>
2015-07-07 17:24:05 +0100

    add support for aside sidebar on pages (fixes: #6)

--- content-page.php
+++ content-page.php
@@ -1,9 +1,19 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<?php
+/**
+ * support for sidebars in pages
+ * If the Sidebar content box (aside Pods field) has any content,
+ * main content is shrunk to 9/12 cols and a 3/12 cols sidebar
+ * is added with the content of the aside field.
+ */
+$pod = pods('page', pods_v('last', 'url'));
+$aside = $pod->display('aside');
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('row'); ?>>
+	<header class="entry-header col-md-12">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content<?php if(!empty($aside)) { echo ' col-md-9'; } else { echo ' col-md-12'; } ?>">
 		<?php the_content(); ?> 
 		<div class="clearfix"></div>
 		<?php
@@ -18,6 +28,12 @@
 		));
 		?>
 	</div><!-- .entry-content -->
+	
+	<?php if(!empty($aside)) : ?>
+	<aside class="col-md-3">
+	<?php echo $aside; ?>
+	</aside>
+	<?php endif; // (!empty($aside)) ?>
 	
 	<footer class="entry-meta">
 		<?php bootstrapBasicEditPostLink(); ?> 
