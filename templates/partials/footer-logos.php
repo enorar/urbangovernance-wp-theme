d1d5d80de9e30a2253cd88798009cf318cb4ed74
andrea rota <a.rota@lse.ac.uk>
2015-07-16 11:16:42 +0100

    make logos respond better to screen size (source images resize needed)

--- templates/partials/footer-logos.php
+++ templates/partials/footer-logos.php
@@ -1,10 +1,11 @@
                                 <div class="container logos">
-                                        <div class="col-xs-12 col-md-10">A collaboration between:<br>
-                                                <a href="https://lsecities.net/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_lsecities.png" alt="LSE Cities"></a>
-                                                <a href="http://unhabitat.org/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_unhabitat.png" alt="UN Habitat"></a>
-                                                <a href="http://www.uclg.org/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_uclg_en.png" alt="UCLG"></a>
+                                        <div class="col-xs-9 row">
+						<div class="col-xs-12">A collaboration between:</div>
+                                                <a class="col-xs-12 col-sm-4" href="https://lsecities.net/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_lsecities.png" alt="LSE Cities"></a>
+                                                <a class="col-xs-12 col-sm-4" href="http://unhabitat.org/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_unhabitat.png" alt="UN Habitat"></a>
+                                                <a class="col-xs-12 col-sm-4" href="http://www.uclg.org/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_uclg_en.png" alt="UCLG"></a>
                                         </div>
-                                        <div class="col-xs-12 col-md-2 right">Supported by:<br>
+                                        <div class="col-xs-3 right">Supported by:<br>
                                                 <a href="http://www.macarthur.org/"><img src="<?php bloginfo('template_directory'); ?>/img/logo_macarthur.png" alt="MacArthur Foundation"></a>
                                         </div>
                                 </div>
